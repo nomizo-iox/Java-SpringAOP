cd87ae533212569cc73546edf4b69a90367623b7
nomizo-iox <nomizo.iox@gmail.com>
2020-02-18 15:05:03 -0600

    added to the Controller, Service, and Repository.

--- src/main/java/com/nomizo/controller/ProductController.java
+++ src/main/java/com/nomizo/controller/ProductController.java
@@ -1,8 +1,31 @@
 package com.nomizo.controller;
 
+import com.nomizo.model.Product;
+import com.nomizo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @PostMapping("/save")
+    public List<Product> saveProduct(@RequestBody List<Product> products) {
+        List<Product> productList = productService.addProduct(products);
+        return productList;
+    }
+
+    @GetMapping("/findProducts")
+    public List<Product> getProducts() {
+        List<Product> products = productService.findAllProducts();
+        return products;
+    }
 
 }
