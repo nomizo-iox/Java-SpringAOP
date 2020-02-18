e588c8e112a552647734d3966c9ba07468a46e3b
nomizo-iox <nomizo.iox@gmail.com>
2020-02-18 13:22:21 -0600

    first commit

---
+++ src/test/java/com/nomizo/SpringAop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomizo.SpringAop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
