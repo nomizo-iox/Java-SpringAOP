b764c9cec20ca403d13d710515c2cce0f9e90fbc
nomizo-iox <nomizo.iox@gmail.com>
2020-02-19 18:21:58 -0600

    Removed all the unecessary code.

--- src/main/java/com/nomizo/coverage/ProductAspect.java
+++ src/main/java/com/nomizo/coverage/ProductAspect.java
@@ -1,6 +1,5 @@
 package com.nomizo.coverage;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nomizo.log.Logging;
@@ -8,8 +7,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
